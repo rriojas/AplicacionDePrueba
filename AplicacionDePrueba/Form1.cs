fbeb666a531a3c61c83b2f87b2a7b2ef5ea41929
Ruben Riojas <rriojas@gmail.com>
2025-08-08 12:55:03 -0600

    Add project files.

---
+++ AplicacionDePrueba/Form1.cs
@@ -0,0 +1,11 @@
+namespace AplicacionDePrueba
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      this.BackColor= System.Drawing.Color.FromArgb(255, 0, 0);
+    }
+  }
+}
